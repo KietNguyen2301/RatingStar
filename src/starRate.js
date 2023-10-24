ba9f81cabf0fa5d7df96e1dec021c3cd84719153
KietNguyen2301 <kietde160233@gmail.com>
2023-10-24 22:22:50 +0700

    ratingstar2

--- src/starRate.js
+++ src/starRate.js
@@ -7,6 +7,10 @@ export default function StarRate() {
     const handleUserRatingChange = (e) => {
         setUserRating(e.target.value);
     };
+
+    const handleStarClick = (value) => {
+        setUserRating(value);
+    };
     return (
         <>
             <div class="container">
@@ -14,16 +18,16 @@ export default function StarRate() {
                     <h2>Star Rating</h2>
                     <div class="center">
                         <fieldset class="rating">
-                            <input type="radio" id="star5" name="rating" value="5" checked={Number(userRating) === 5} /><label for="star5" class="full" title="Awesome"></label>
-                            <input type="radio" id="star4.5" name="rating" value="4.5" checked={Number(userRating) === 4.5}/><label for="star4.5" class="half"></label>
-                            <input type="radio" id="star4" name="rating" value="4" checked={Number(userRating) === 4}/><label for="star4" class="full"></label>
-                            <input type="radio" id="star3.5" name="rating" value="3.5" checked={Number(userRating) === 3.5}/><label for="star3.5" class="half"></label>
-                            <input type="radio" id="star3" name="rating" value="3" checked={Number(userRating) === 3}/><label for="star3" class="full"></label>
-                            <input type="radio" id="star2.5" name="rating" value="2.5" checked={Number(userRating) === 2.5}/><label for="star2.5" class="half"></label>
-                            <input type="radio" id="star2" name="rating" value="2" checked={Number(userRating) === 2}/><label for="star2" class="full"></label>
-                            <input type="radio" id="star1.5" name="rating" value="1.5" checked={Number(userRating) === 1.5}/><label for="star1.5" class="half"></label>
-                            <input type="radio" id="star1" name="rating" value="1" checked={Number(userRating) === 1}/><label for="star1" class="full"></label>
-                            <input type="radio" id="star0.5" name="rating" value="0.5" checked={Number(userRating) === 0.5}/><label for="star0.5" class="half"></label>
+                            <input type="radio" id="star5" name="rating" value="5" checked={Number(userRating) === 5} onClick={() => handleStarClick(5)}/><label for="star5" class="full" title="Awesome"></label>
+                            <input type="radio" id="star4.5" name="rating" value="4.5" checked={Number(userRating) === 4.5} onClick={() => handleStarClick(4.5)}/><label for="star4.5" class="half"></label>
+                            <input type="radio" id="star4" name="rating" value="4" checked={Number(userRating) === 4} onClick={() => handleStarClick(4)}/><label for="star4" class="full"></label>
+                            <input type="radio" id="star3.5" name="rating" value="3.5" checked={Number(userRating) === 3.5} onClick={() => handleStarClick(3.5)}/><label for="star3.5" class="half"></label>
+                            <input type="radio" id="star3" name="rating" value="3" checked={Number(userRating) === 3} onClick={() => handleStarClick(3)}/><label for="star3" class="full"></label>
+                            <input type="radio" id="star2.5" name="rating" value="2.5" checked={Number(userRating) === 2.5} onClick={() => handleStarClick(2.5)}/><label for="star2.5" class="half"></label>
+                            <input type="radio" id="star2" name="rating" value="2" checked={Number(userRating) === 2} onClick={() => handleStarClick(2)}/><label for="star2" class="full"></label>
+                            <input type="radio" id="star1.5" name="rating" value="1.5" checked={Number(userRating) === 1.5} onClick={() => handleStarClick(1.5)}/><label for="star1.5" class="half"></label>
+                            <input type="radio" id="star1" name="rating" value="1" checked={Number(userRating) === 1} onClick={() => handleStarClick(1)}/><label for="star1" class="full"></label>
+                            <input type="radio" id="star0.5" name="rating" value="0.5" checked={Number(userRating) === 0.5} onClick={() => handleStarClick(0.5)}/><label for="star0.5" class="half"></label>
                         </fieldset>
                     </div>
                 </div>
@@ -41,6 +45,11 @@ export default function StarRate() {
                         onChange={handleUserRatingChange}
                     />
                 </div>
+            </div>
+
+            <div className="user-rating">
+                {/* Hiển thị số ở đây */}
+                {userRating && <p>User Rating: {(userRating)}</p>}
             </div>
         </>
     );
